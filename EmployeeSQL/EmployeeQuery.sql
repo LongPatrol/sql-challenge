0e03228db6d3861cc934b7217a1c51f9c5d00c65
MSabo <72773063+LongPatrol@users.noreply.github.com>
2021-01-14 13:00:44 -0500

    Created first 3 tables, setting up keys.

--- EmployeeSQL/EmployeeQuery.sql
+++ EmployeeSQL/EmployeeQuery.sql
@@ -1,18 +1,34 @@
 CREATE TABLE titles (
-	title_id VARCHAR(255) PRIMARY KEY
-	, title VARCHAR(255))
+	title_id VARCHAR(255) PRIMARY KEY NOT NULL
+	, title VARCHAR(255)
+	)
 ;
 
 CREATE TABLE salaries(
-	emp_no VARCHAR(255) PRIMARY KEY
-	, salary INTEGER)
+	emp_no VARCHAR(255) PRIMARY KEY NOT NULL 
+	, salary INTEGER
+	)
 ;
 
 CREATE TABLE employees(
-	emp_no INT PRIMARY KEY
-	, emp_title VARCHAR(255)
+	emp_no INT PRIMARY KEY NOT NULL
+	, emp_title VARCHAR(255) NOT NULL
+	, FOREIGN KEY (emp_title) REFERENCES titles(title_id)
 	, birth_date DATE
 	, first_name VARCHAR(255)
 	, last_name VARCHAR(255)
 	, sex VARCHAR(1)
-	, hire_date DATE)
+	, hire_date DATE
+	)
+;
+
+
+--need to create department table first
+/*
+CREATE TABLE dept_emp(
+	emp_no INTEGER NOT NULL
+	, dept_no VARCHAR(255) NOT NULL
+	, FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	, FOREIGN KEY (dept_no) REFERENCES 
+	)
+*/
